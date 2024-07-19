badeba4b69891f07e98e26f24079f1c9da826848
Spnism <pimsunisa16@gmail.com>
2024-07-19 01:12:08 -0800

    ...

--- ep.1/index.ts
+++ ep.1/index.ts
@@ -6,3 +6,10 @@ var wanna_go_home : boolean = true
 console.log(firstname)
 console.log(y)
 console.log(wanna_go_home)
+----------------------------------------------
+const a:number = 5 
+const b:number = 6
+
+console.log(a===b)
+console.log(a>b)
+console.log(a<=b)
