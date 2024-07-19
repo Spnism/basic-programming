356a3e562ea309c80a91b2b18f217897cac37ea0
Spnism <pimsunisa16@gmail.com>
2024-07-19 01:27:52 -0800

    ...

--- ep.1/index.ts
+++ ep.1/index.ts
@@ -1,7 +1,7 @@
-const firstname : string = 'sunisa_kaewchompoo'
+const firstName: string = 'sunisa_kaewchompoo'
 var y: number = 10
 y = 20
-var wanna_go_home : boolean = true
+var wanna_go_home: boolean = true
 
 console.log(firstname)
 console.log(y)
