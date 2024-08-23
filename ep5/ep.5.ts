fd3f4fe3cb57e3e128323d2eb89a645eaec48531
Spnism <pimsunisa16@gmail.com>
2024-08-23 14:23:05 +0700

    ...

---
+++ ep5/ep.5.ts
@@ -0,0 +1,23 @@
+let sum: number = 0
+let i:  number = 1
+
+do {
+    sum = sum + i 
+    i++
+}while (i <=31)
+
+console.log('Total:',sum)
+
+let sum: number = 0
+for (let i = 1; i <= 31; i++){
+    sum = sum + i
+}
+console.log('Total:',sum)
+
+let sum: number = 0
+let i: number = 1
+while ( i <= 31){
+    sum = sum + i
+    i++
+}
+console.log('Total:',sum)
