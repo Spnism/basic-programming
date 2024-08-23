14cc53337c6333613ee947b1f596110a5c6e0dae
Spnism <pimsunisa16@gmail.com>
2024-08-23 14:42:23 +0700

    ...

--- ep5/ep.5.ts
+++ ep5/ep.5.ts
@@ -1,23 +1,49 @@
-let sum: number = 0
-let i:  number = 1
+// let sum: number = 0
+// let i:  number = 1
 
-do {
-    sum = sum + i 
-    i++
-}while (i <=31)
+// do {
+//     sum = sum + i 
+//     i++
+// }while (i <=31)
 
-console.log('Total:',sum)
+// console.log('Total:',sum)
 
-let sum: number = 0
-for (let i = 1; i <= 31; i++){
-    sum = sum + i
-}
-console.log('Total:',sum)
+// let sum: number = 0
+// for (let i = 1; i <= 31; i++){
+//     sum = sum + i
+// }
+// console.log('Total:',sum)
 
-let sum: number = 0
-let i: number = 1
-while ( i <= 31){
-    sum = sum + i
-    i++
-}
-console.log('Total:',sum)
+// let sum: number = 0
+// let i: number = 1
+// while ( i <= 31){
+//     sum = sum + i
+//     i++
+// }
+// console.log('Total:',sum)
+
+// let count:number = 0 
+
+// while(true) {
+//     console.log ('โสดว้อย')
+//     count++
+//     if (count === 10){
+        
+//     }
+// }
+
+// //for (let i = 1; i<= 20; i++) {
+// //    console.log('ฉันอายุ'+i+'ปี')
+// //}
+
+// //let i : number =1
+// //while(i <= 20){
+// //    console.log('ฉันอายุ'+i+'ปี')
+// //    i++
+// //}while (i <= 20)
+
+// let i : number = 1
+// while (i <= 20) {
+//     console.log('ฉันอายุ'+i+'ปี')
+//     i++
+// }
