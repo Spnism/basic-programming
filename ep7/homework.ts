c48eab62bf44f162973e3eda6f2884576f45d6d8
Spnism <pimsunisa16@gmail.com>
2024-09-06 14:50:35 +0700

    ...

---
+++ ep7/homework.ts
@@ -0,0 +1,13 @@
+const perSon ={
+    name : 'kub pre',
+    addessNowNo : '001',
+    villageNo : '001',
+    street : 'mairu',
+    district : 'kub',
+    county: 'pre',
+    province: 'bormee',
+    zipCode : '65238',
+    phone : '080*******',
+    emaill : 'kub_pre@gmail.com'
+}
+console.log (perSon)
